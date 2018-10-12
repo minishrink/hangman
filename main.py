978c3e3d37c548ffdcb45a4f5eac607ef2418576
minishrink <mathur_akanksha@hotmail.com>
2018-10-13 00:28:30 +0100

    Hide answer text for posix systems
    Clear terminal for Windows systems
    
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

--- main.py
+++ main.py
@@ -2,30 +2,39 @@ from hangman import Hangman
 import sys
 from os import system, name
 
+def secret_string():
+    system("stty -echo")
+    string = str(input())
+    system("stty echo")
+    print("\n")             # fix formatting
+    return string
+
+def hide_answer():
+    print("Enter hangman answer: ")
+    # windows system clear terminal
+    if name == 'nt':
+        word = str(input())
+        _ = system('cls')
+        return word
+    # posix clear terminal
+    elif name == 'posix':
+        return secret_string()
+
 def make_game():
     rules = """
     Start a game of Hangman by entering a word,
     then a max number of guesses the player is allowed before losing
     """
     print(rules)
-    word = str(input("Enter a word: "))
+    word = hide_answer()
     guesses = int(input("Enter max number of guesses: "))
     return Hangman(word, guesses)
-
-def clear():
-    # windows system clear terminal
-    if name == 'nt':
-        _ = system('cls')
-    # posix clear terminal
-    elif name == 'posix':
-        _ = system('clear')
 
 def main():
     try:
         game = Hangman(sys.argv[1], int(sys.argv[2]))
     except:
         game = make_game()
-    clear()
     game.play()
 
 if (__name__=="__main__"):
